2d3fe6ee3e0703dad0da0e8f97d53e2cdf22145d
krlinhossoares <krlinhos.soares@gmail.com>
2026-04-17 07:37:21 -0300

    Create Entidade.js with Entidade class

---
+++ Entidade.js
@@ -0,0 +1,15 @@
+class Entidade {
+    constructor(id, isCliente, isFornecedor, isTransportador, nomeRazao, nomeFantasia, tpPessoa, cnpjcpf, indIe, Ie, ativo) {
+        this.id = id;
+        this.isCliente = isCliente;
+        this.isFornecedor = isFornecedor;
+        this.isTransportador = isTransportador;
+        this.nomeRazao = nomeRazao;
+        this.nomeFantasia = nomeFantasia;
+        this.tpPessoa = tpPessoa;
+        this.cnpjcpf = cnpjcpf;
+        this.indIe = indIe;
+        this.Ie = Ie;
+        this.ativo = ativo;
+    }
+}
